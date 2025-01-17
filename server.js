9895eee4429ce7bad4b3a232c1c038185fb2640b
jinsol24 <solsoli2468@gmail.com>
2025-01-17 14:44:41 +0900

    #2 Test commit on develop branch2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, CICD World.'));
+app.get('/', (req, res) => res.send('Hello, CICD World!'));
 app.get('/health', (req, res) => res.status(200).send({ status: 'UP' }));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
